8d5ad8c1bfa96bd88a0a7247aad691ba2dd3617c
Frank <117088885+FrankWW07@users.noreply.github.com>
2023-09-05 22:35:14 -0600

    Update verify.sh commit

--- verify.sh
+++ verify.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ -f "f1" ]; then
-  permisos = stat -c "%A" "$1"
+# verificar si el archivo ingresado existe o no
+if [ -f "f1" ]; then 
+  permisos = stat -c "%A" "$1" # si el archivo existe entonces se guarda el valor de los permisos en la variable 'permisos'
   echo "El archivo '$f1' existe y tiene los permisos: $permisos"
 
 else
-  echo "Error: el archivo no existe, favor intentar de nuevo."
+  echo "Error: el archivo no existe, favor intentar de nuevo."  # si el archivo no existe imprime mensaje y manda un valor a la salida
   exit 0
 
 fi
